1ae51f0f38bfe68f53c814afe32152f7467f5240
Артём <125689953+artem-vildanov@users.noreply.github.com>
2023-12-18 14:23:03 +0700

    Update osLab1.cpp

--- osLab1.cpp
+++ osLab1.cpp
@@ -4,43 +4,48 @@
 
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-int ready = 0;
+bool ready = false;
 
 void* producer(void*) {
-    while (true) {
-        pthread_mutex_lock(&lock);
-        if (ready == 1) {
-            pthread_mutex_unlock(&lock);
-            continue;
-        }
-        ready = 1;
-        std::cout << "Поставщик: отправляю событие" << std::endl;
-        pthread_cond_signal(&cond1);
-        pthread_mutex_unlock(&lock);
-        sleep(1); // Задержка в 1 секунду
-    }
-    return nullptr;
+
+	pthread_mutex_lock(&lock);
+	if (ready == true) {
+		pthread_mutex_unlock(&lock);
+	}
+	
+	std::cout << "Поставщик: отправляю событие" << std::endl;
+	sleep(1); // выполняем действие
+	
+	ready = true;
+	
+	pthread_cond_signal(&cond1);
+	pthread_mutex_unlock(&lock);
+
+
+	return nullptr;
 }
 
 void* consumer(void*) {
-    while (true) {
-        pthread_mutex_lock(&lock);
-        while (ready == 0) {
-            pthread_cond_wait(&cond1, &lock);
-        }
-        ready = 0;
-        std::cout << "Потребитель: получено событие" << std::endl;
-        pthread_mutex_unlock(&lock);
-    }
-    return nullptr;
+	
+	pthread_mutex_lock(&lock);
+	while (ready == false) {
+		pthread_cond_wait(&cond1, &lock);
+	}
+	ready = false;
+	std::cout << "Потребитель: получено событие" << std::endl;
+	pthread_mutex_unlock(&lock);
+
+	return nullptr;
 }
 
 int main() {
     pthread_t producerThread, consumerThread;
 
-    // Запускаем потоки
     pthread_create(&producerThread, nullptr, producer, nullptr);
     pthread_create(&consumerThread, nullptr, consumer, nullptr);
+	
+    pthread_join(producerThread, nullptr);
+    pthread_join(consumerThread, nullptr);
 
     return 0;
 }
